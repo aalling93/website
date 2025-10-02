ead2ec34cda00d8aa9e3efbdb951023543e3b629
Mac <kaaso@space.dtu.dk>
2025-10-02 12:02:51 +0200

    login with js instead.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,13 @@
-//  Currently empty.
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if(username === 'test123' && password === 'pw123') {
+    window.location.href = 'test_page.html';
+  } else {
+    const errorMessage = document.getElementById('errorMessage');
+    errorMessage.style.display = 'block';
+    errorMessage.textContent = 'Invalid username or password';
+  }
+});
